ff1afa922ee6142a8352c02c7d82bb9f5c2da3ab
Jamal Whorms-MonDesir <jamal@letswayfare.com>
2022-09-09 10:23:17 +0100

    Add Location services function to user entry

--- screens/UserEntryScreens/CreateUserLocScreen.js
+++ screens/UserEntryScreens/CreateUserLocScreen.js
@@ -1,12 +1,36 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import * as Location from 'expo-location';
 
 const CreateUserLocScreen = () => {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  let text = 'Waiting..';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
+
   return (
     <View>
-      <Text>CreateUserLocScreen</Text>
+      <Text>{location}</Text>
     </View>
-  )
+  );
 }
 
 export default CreateUserLocScreen
