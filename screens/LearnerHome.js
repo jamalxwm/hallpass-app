b46b3cf297005eac734243dacd84768e33d6cab2
leoniephelps <leoniephelps96@gmail.com>
2022-09-09 14:40:41 +0100

    add single tutor navigation

--- screens/LearnerHome.js
+++ screens/LearnerHome.js
@@ -9,22 +9,25 @@ import {
 import { React, useEffect, useState } from "react";
 import { db } from "../firebase";
 import { getDocs, collection, query, where } from "firebase/firestore";
-import { Chip } from "react-native-paper";
+import { Chip, Card, Title } from "react-native-paper";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-export const LearnerHome = ({navigation}) => {
+export const LearnerHome = ({ navigation }) => {
   const [tutors, setTutors] = useState([]);
 
   const tutorsCollectionRef = collection(db, "Tutors");
 
+  const getAllTutors = async () => {
+    const data = await getDocs(tutorsCollectionRef);
+    const myArr = [];
+    data.forEach((doc) => {
+      myArr.push(doc.data());
+    });
+    setTutors(myArr);
+  };
+
   const getTutors = async (skill) => {
-    if (!skill) {
-      const data = await getDocs(tutorsCollectionRef);
-      const myArr = [];
-      data.forEach((doc) => {
-        myArr.push(doc.data(), doc.id);
-      });
-      setTutors(myArr);
-    }
     const testQuery = query(
       tutorsCollectionRef,
       where("skills", "array-contains", skill)
@@ -32,13 +35,13 @@ export const LearnerHome = ({navigation}) => {
     const data = await getDocs(testQuery);
     const myArr = [];
     data.forEach((doc) => {
-      myArr.push(doc.data(), doc.id);
+      myArr.push(doc.data());
     });
     setTutors(myArr);
   };
 
   useEffect(() => {
-    getTutors();
+    getAllTutors();
   }, []);
 
   return (
@@ -46,7 +49,7 @@ export const LearnerHome = ({navigation}) => {
       <Chip
         icon="information"
         style={styles.skillfilter}
-        onPress={() => getTutors()}
+        onPress={() => getAllTutors()}
       >
         All
       </Chip>
@@ -80,17 +83,18 @@ export const LearnerHome = ({navigation}) => {
       </Chip>
       {tutors.map((tutor) => {
         return (
-          <View>
-            <Image style={styles.tinyLogo} source={{ uri: tutor.image }} />
-            <Text>{tutor.firstname}</Text>
-            <Text>{tutor.skills}</Text>
-            <Button
-              title="view tutor"
-              onPress={(e) => {
-                e.preventDefault() && navigation.replace("SingleTutor");
+          <Card>
+            <Card.Cover style={styles.tinyLogo} source={{ uri: tutor.image }} />
+            <Card.Content>
+              <Title>{tutor.firstname}</Title>
+              <Text>{tutor.skills}</Text>
+            </Card.Content>
+            <Chip
+              onPress={() => {
+                navigation.navigate("SingleTutor");
               }}
             />
-          </View>
+          </Card>
         );
       })}
     </ScrollView>
@@ -104,5 +108,8 @@ const styles = StyleSheet.create({
   },
   skillfilter: {
     width: 150,
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
