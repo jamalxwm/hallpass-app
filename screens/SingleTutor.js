a5b2dc02aaef0a4d5adf0f75b8913503d60326c2
leoniephelps <leoniephelps96@gmail.com>
2022-09-10 12:28:28 +0100

    pass single tutor data as params

--- screens/SingleTutor.js
+++ screens/SingleTutor.js
@@ -1,16 +1,17 @@
-import { StyleSheet, Text, View, Image } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 
 const SingleTutor = ({
   route: {
-    params: { tutor:{tutorData} },
+    params: {
+      tutor: { tutorData },
+    },
   },
 }) => {
   console.log(tutorData);
   return (
     <View>
       <Text>SingleTutor</Text>
-      <Image source={{ uri: tutor.tutorData.image }} />
     </View>
   );
 };
