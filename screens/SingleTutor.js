466f62dff25892a3a309bf947d5a5eebd9f628a9
leoniephelps <leoniephelps96@gmail.com>
2022-09-10 12:28:12 +0100

    pass single tutor data as props

--- screens/SingleTutor.js
+++ screens/SingleTutor.js
@@ -1,14 +1,20 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, Image } from "react-native";
+import React from "react";
 
-const SingleTutor = () => {
+const SingleTutor = ({
+  route: {
+    params: { tutor:{tutorData} },
+  },
+}) => {
+  console.log(tutorData);
   return (
     <View>
       <Text>SingleTutor</Text>
+      <Image source={{ uri: tutor.tutorData.image }} />
     </View>
-  )
-}
+  );
+};
 
-export default SingleTutor
+export default SingleTutor;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
