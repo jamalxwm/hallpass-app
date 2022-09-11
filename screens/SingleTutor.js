63df7bc5565bdaa36717df57ea38ba940063a758
leoniephelps <leoniephelps96@gmail.com>
2022-09-11 15:27:28 +0100

    add tutor information with conditional rendering

--- screens/SingleTutor.js
+++ screens/SingleTutor.js
@@ -1,5 +1,5 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
+import React, { useState } from "react";
 
 const SingleTutor = ({
   route: {
@@ -8,14 +8,33 @@ const SingleTutor = ({
     },
   },
 }) => {
-  console.log(tutorData);
+  const reviews = tutorData.reviews;
+
   return (
     <View>
-      <Text>SingleTutor</Text>
+      <Image style={styles.tinyLogo} source={{ uri: tutorData.image }} />
+      <Text>
+        {tutorData.firstname} {tutorData.lastname}
+      </Text>
+      <Text>{tutorData.skills}</Text>
+      {tutorData.inperson && <Text>in person</Text>}
+      {tutorData.virtual && <Text>virtual</Text>}
+      <Text>{tutorData.bio}</Text>
+
+      {reviews && <Text>Reviews:</Text>}
+      {reviews &&
+        reviews.map((review) => {
+          return <Text key={review}>{review}</Text>;
+        })}
     </View>
   );
 };
 
 export default SingleTutor;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  tinyLogo: {
+    width: 200,
+    height: 200,
+  },
+});
