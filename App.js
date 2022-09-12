1a92b5f59582601c8bf890c9b646d9f30e457284
Jamal Whorms-MonDesir <jamal@letswayfare.com>
2022-09-12 10:06:11 +0100

    Add chat functionality

--- App.js
+++ App.js
@@ -1,48 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LoginScreen from './screens/UserEntryScreens/LoginScreen';
-import HomeScreen from './screens/HomeScreen';
-import { useFonts } from 'expo-font';
-import UserEntryStack from './screens/UserEntryStack';
-import { UserContext } from './src/contexts/user';
-import { useState } from 'react';
-import SingleTutor from "./screens/SingleTutor";
-import { LearnerHome } from "./screens/LearnerHome";
-
-const Stack = createNativeStackNavigator();
-
-export default function App() {
-  const [loggedInUser, setLoggedInUser] = useState([]);
-
-  return (
-    <UserContext.Provider value={{ loggedInUser, setLoggedInUser }}>
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen
-            options={{ headerShown: false }}
-            name="Login"
-            component={LoginScreen}
-          />
-          <Stack.Screen
-            name="UserEntry"
-            options={{ headerShown: false }}
-            component={UserEntryStack}
-          />
-          <Stack.Screen name="Home" component={LearnerHome} />
-          <Stack.Screen name="SingleTutor" component={SingleTutor} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </UserContext.Provider>
-  );
+import { GiftedChat } from 'react-native-gifted-chat';
+export default class App extends React.Component {
+  state = {
+    messages: [
+      {
+        _id: 1,
+        text: 'Hello developer',
+        createdAt: new Date(),
+      },
+    ],
+  };
+  render() {
+    return (
+      <View style={styles.container}>
+        <GiftedChat messages={this.state.messages} />
+      </View>
+    );
+  }
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
   },
 });
