f2e352432b08ae5689ee2be233146b58669316de
leoniephelps <leoniephelps96@gmail.com>
2022-09-15 11:02:35 +0100

    add header changes

--- App.js
+++ App.js
@@ -1,16 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LoginScreen from './screens/UserEntryScreens/LoginScreen';
-import HomeScreen from './screens/HomeScreen';
-import UserEntryStack from './screens/UserEntryStack';
-import { UserContext } from './src/contexts/user';
-import { useState } from 'react';
-import SingleTutor from './screens/SingleTutor';
-import { LearnerHome } from './screens/LearnerHome';
-import MapScreen from './screens/MapScreen';
-
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import LoginScreen from "./screens/UserEntryScreens/LoginScreen";
+import HomeScreen from "./screens/HomeScreen";
+import UserEntryStack from "./screens/UserEntryStack";
+import { UserContext } from "./src/contexts/user";
+import { useState } from "react";
+import SingleTutor from "./screens/SingleTutor";
+import { LearnerHome } from "./screens/LearnerHome";
+import MapScreen from "./screens/MapScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -36,8 +35,16 @@ export default function App() {
             options={{ headerShown: false }}
             component={LearnerHome}
           />
-          <Stack.Screen name="SingleTutor" component={SingleTutor} />
-          <Stack.Screen name="MapScreen" component={MapScreen} />
+          <Stack.Screen
+            name="SingleTutor"
+            options={{ headerShown: false }}
+            component={SingleTutor}
+          />
+          <Stack.Screen
+            name="MapScreen"
+            options={{ headerShown: false }}
+            component={MapScreen}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </UserContext.Provider>
@@ -47,8 +54,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
